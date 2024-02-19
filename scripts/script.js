46ef9e64907755f4f48058093c07bf8a40264966
afroza-sultana <afrozasultana9898@gmail.com>
2024-02-19 19:35:45 +0600

    discount price update

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 const allBtn = document.getElementsByClassName('btn');
 let count = 0;
 let seatLeft = 8;
-for (const btn of allBtn){
-    btn.addEventListener('click',function(e){
+for (const btn of allBtn) {
+    btn.addEventListener('click', function (e) {
         count = count + 1;
         seatLeft = seatLeft - 1;
         setInnerText('seat-num', count);
         setInnerText('seat-left', seatLeft);
         const setBgColor = e.target.id;
         addBackgroundColor(setBgColor)
-        const seatDetails= document.getElementById('selected-seat-details');
+        const seatDetails = document.getElementById('selected-seat-details');
         const div = document.createElement('div');
         div.innerHTML = `
         <p>${setBgColor}</p>
         <p>Economoy</p>
         <span>550</span>
     `;
-    div.classList.add('flex', 'justify-between');
-    seatDetails.appendChild(div);
-        const getPrice= count * 550;
+        div.classList.add('flex', 'justify-between');
+        seatDetails.appendChild(div);
+        const getPrice = count * 550;
         const totalPrice = document.getElementById('total-price');
         totalPrice.innerText = getPrice;
         const grandTotal = document.getElementById('grand-total');
@@ -26,10 +26,54 @@ for (const btn of allBtn){
     })
 }
 
-function setInnerText (id, value){
+function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
-function addBackgroundColor(id){
+function addBackgroundColor(id) {
     const element = document.getElementById(id);
-    element.classList.add('bg-[#1DD100]','text-white')
+    element.classList.add('bg-[#1DD100]', 'text-white')
 }
+
+const nextButton = document.getElementById('next-btn');
+const numberInput = document.getElementById('number-input');
+const inputValue = parseInt(numberInput.value);
+
+
+document.getElementById('apply-btn').addEventListener('click', function (e) {
+    const input = document.getElementById('cupon-input');
+    const grandTotal = document.getElementById('grand-total');
+    const inputValue = input.value;
+    if (inputValue === "NEW15") {
+        const discountPrice = parseInt(grandTotal.innerText) * .15;
+        const discount = document.getElementById('discount');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p>discount</p>
+        <p>${discountPrice}</p>   
+    `;
+        div.classList.add('flex', 'justify-between', 'text-lg', 'mt-4');
+        discount.appendChild(div);
+        const price = parseInt(grandTotal.innerText) - discountPrice;
+        grandTotal.innerText = price;
+    } else if (inputValue === 'Couple 20') {
+        const discountPrice = parseInt(grandTotal.innerText) * .20;
+        const discount = document.getElementById('discount');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p>discount</p>
+        <p>${discountPrice}</p>   
+    `;
+        div.classList.add('flex', 'justify-between', 'text-lg', 'mt-4');
+        discount.appendChild(div);
+        const price = parseInt(grandTotal.innerText) - discountPrice;
+        grandTotal.innerText = price;
+    }
+    input.classList.add('hidden');
+    e.target.classList.add('hidden');
+
+})
+
+
+
+
+
